42b2d57386892cee96161215b8abcb05f3a58169
Shahed Rahman <153431638+shahed-dev01@users.noreply.github.com>
2025-08-03 17:55:20 +0600

    File upload.

---
+++ save_mysql_01.sql
@@ -0,0 +1,20 @@
+SELECT * 
+FROM parks_and_recreation.employee_demographics;
+
+SELECT first_name
+FROM parks_and_recreation.employee_demographics;
+
+SELECT last_name
+FROM parks_and_recreation.employee_demographics;
+
+SELECT age, gender
+FROM parks_and_recreation.employee_demographics;
+
+SELECT birth_date
+FROM parks_and_recreation.employee_demographics;
+
+SELECT age, age + 10
+FROM parks_and_recreation.employee_demographics;
+
+SELECT DISTINCT gender
+FROM parks_and_recreation.employee_demographics;
